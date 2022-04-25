7773469a6faebc1f9c7ceea261d84e23fef47f18
muragoness <102125857+muragoness@users.noreply.github.com>
2022-04-25 20:46:17 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,79 @@
+class PrototypesController < ApplicationController
+
+  
+  before_action :authenticate_user!, except: [:index, :show]
+
+  
+  
+  def index
+    @prototypes = Prototype.includes(:user)
+
+  end
+
+
+  def new 
+    @prototype = Prototype.new
+  end
+
+
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to prototypes_path
+    else 
+      render :new
+    end
+  
+  end
+
+
+  def show
+    @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+
+
+
+  end
+
+  def update
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype)
+    else
+      redirect_to prototype_path
+    end
+
+  end
+
+  def destroy
+    if @prototype.destoroy
+      redirect_to prototype_path
+    else 
+      redirect_to prototype_path
+    end
+
+  end
+
+
+
+  private 
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+
+    unless user_signed_in?
+      redirect_to action: :edit
+    end
+  end
+
+
+
+end
